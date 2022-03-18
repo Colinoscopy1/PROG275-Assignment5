ea4dbbb2c8a129d9c2216bb8cc8bfd0a73275ebe
colin.carrier <colin.carrier@B231-10>
2022-03-18 14:42:03 -0600

    Added forms for the login and user creation

---
+++ ClassLibrary/BusinessLayer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    public  class BusinessLayer
+    {
+        public static User LoggedInUser = null;
+
+        public static bool Login(string username, string password)
+        {
+            bool ret = false;
+
+            using (DBContext db = new DBContext())
+            {
+                int count = db.Users.Where(x => x.Username == username && x.Password == password).Count();
+                if (count == 1)
+                {
+                    ret = true;
+                    LoggedInUser = db.Users.Where(x => x.Username == username && x.Password == password).FirstOrDefault();
+
+                }
+            }
+            return ret;
+        }
+
+    }
+}
