bd72acf789238326f7f5853ac412730fe5b14b7b
Nicholas Regehr <Nicholas.Regehr@mymhc.ca>
2022-03-18 14:20:47 -0600

    added dbcontext

---
+++ ClassLibrary/DBContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
+
+
+namespace ClassLibrary
+{
+    class DBContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Ticket> Tickets { get; set; }
+        public DbSet<Technician> Technicians { get; set; }
+        public DbSet<Admin> Admins { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(@"server=localhost,1434;database=HelpDeskDB;user id=sa;password=P@ssword!");
+        }
+    }
+}
